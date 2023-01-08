df9f01a6ddb8acf2d8a839bfa88703c25734c94e
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-01-08 18:18:03 -0300

    Adicionando um plugin para realizar alterações no html gerado e no ebook gerado

---
+++ cotuba/src/main/java/cotuba/plugin/Plugin.java
@@ -0,0 +1,42 @@
+package cotuba.plugin;
+
+import cotuba.domain.Capitulo;
+import cotuba.domain.Ebook;
+
+import java.util.ServiceLoader;
+
+public interface Plugin {
+
+    /**
+     * Método que poderá ser utilizado para customizações a
+     * serem aplicadas após a renderização do HTML dos
+     * capítulos.
+     * @param html String que representa o html renderizado de cada capítulo
+     * @return html modificado.
+     */
+    String aposRenderizacao(String html);
+
+    /**
+     * Método para customizações a serem feitas depois da
+     * geração do ebook.
+     * @param ebook
+     */
+    void aposGeracao(Ebook ebook);
+
+    static void renderizou(Capitulo capitulo) throws InterruptedException {
+        ServiceLoader.load(Plugin.class)
+            .forEach(plugin -> {
+                String html = capitulo.getConteudoHTML();
+                String htmlModificado = plugin.aposRenderizacao(html);
+                capitulo.setConteudoHTML(htmlModificado);
+            });
+      System.out.println("Aplicou a estilização!");
+      Thread.sleep(2000);
+    }
+
+    static void gerou(Ebook ebook) {
+      ServiceLoader.load(Plugin.class)
+          .forEach(plugin -> plugin.aposGeracao(ebook));
+    }
+
+}
