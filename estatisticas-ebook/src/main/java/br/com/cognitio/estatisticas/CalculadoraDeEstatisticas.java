5f8ef727b77c091556de380fe94976893465430e
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-01-13 13:57:51 -0300

    Removendo acentos e pontuação, e deixando maiúsculo o texto do ebook para contagem de palavras

--- estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/CalculadoraDeEstatisticas.java
+++ estatisticas-ebook/src/main/java/br/com/cognitio/estatisticas/CalculadoraDeEstatisticas.java
@@ -6,6 +6,12 @@ import cotuba.plugin.Plugin;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.text.Normalizer;
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class CalculadoraDeEstatisticas implements Plugin {
 
   @Override
@@ -15,18 +21,42 @@ public class CalculadoraDeEstatisticas implements Plugin {
 
   @Override
   public void aposGeracao(Ebook ebook) {
+    Map<String, Integer> contagemDePalavras = new TreeMap<>();
+
     for (Capitulo capitulo : ebook.getCapitulos()) {
       String html = capitulo.getConteudoHTML();
 
       Document doc = Jsoup.parse(html);
 
       String textoDoCapitulo = doc.body().text();
+      String textoDoCapituloSemPontuacao =
+          textoDoCapitulo.replaceAll("\\p{Punct}", " ");
 
-      String[] palavras = textoDoCapitulo.split("\\s+");
+      String decomposta = Normalizer.normalize(textoDoCapituloSemPontuacao, Normalizer.Form.NFD);
+      String textoDoCapituloSemAcentos = decomposta.replaceAll("[^\\p{ASCII}]", "");
 
-      for(String palavra : palavras) {
-        System.out.println(palavra);
+
+
+      String[] palavras = textoDoCapituloSemAcentos.split("\\s+");
+
+      for (String palavra : palavras) {
+        String emMaiusculas = palavra.toUpperCase();
+        contagemDePalavras.put(emMaiusculas, 1);
       }
+
+      for (Map.Entry<String, Integer> contagem : contagemDePalavras.entrySet()) {
+        String palavra = contagem.getKey();
+
+        Integer ocorrencias = contagem.getValue();
+
+        System.out.println(palavra + ": " + ocorrencias);
+      }
+
+    }
+    try {
+      Thread.sleep(20000);
+    } catch (Exception ex) {
+      System.out.println("Something went wrong!");
     }
   }
 
